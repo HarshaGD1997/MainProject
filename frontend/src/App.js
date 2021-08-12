e34c9c5e057d755f2975dbdb72f6814e5d7c610e
harsha <harshagd695@gmail.com>
2021-08-12 23:13:41 +0530

    Second Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-
+import { Container } from 'react-bootstrap';
 import './App.css';
+import './index.css'
+import Header from './components/Header';
+import Footer from './components/Footer'; 
 
 const App = () => {
   return (
     <>
-      <h1>Welcome</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome</h1>
+          </Container>
+      </main>
+      <Footer />
     </>
   );
 }
