40d76d4be442aeb8113a366c678ed467a011f0f4
harsha <harshagd695@gmail.com>
2021-08-13 12:53:18 +0530

    commit 6

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Yamaha Acoustic Guitar',
+    image: '/images/YamahaAcoustic.jpg',
+    description:
+      'High Quality Acoustic Guitar',
+    brand: 'Yamaha',
+    category: 'Acoustic',
+    price: 8299.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Epiphone Electric Guitar',
+    image: '/images/epiphone.jpg',
+    description:
+      'Exclusive Product from Epiphone',
+    brand: 'Epiphone',
+    category: 'Electric',
+    price: 55599.99,
+    countInStock: 7,
+    rating: 4.9,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Suzuki Cello',
+    image: '/images/SuzukiCello.jpg',
+    description:
+      'Premium Classic Cello',
+    brand: 'Suzuki',
+    category: 'Acoustic',
+    price: 99929.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Yamaha Drum Kit',
+    image: '/images/YamahaDrum.jpg',
+    description:
+      'Ultimate Drum kit',
+    brand: 'Yamaha',
+    category: 'Drums',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Yamaha Piano',
+    image: '/images/YamahaPiano.jpg',
+    description:
+      'Yamaha Piano for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Hohner Harmonica',
+    image: '/images/HohnerHarmonica.jpg',
+    description:
+      'Meet the Hohner Harmonica',
+    brand: 'Hohner',
+    category: 'Harmonica',
+    price: 2229.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports = products
